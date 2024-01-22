5ff579b0b188f2a54f318f9620cfd5d090a7b3da
T1melord1 <vlad.bobrenok.2001@gmail.com>
2024-01-22 13:16:10 +0300

    Создания объет petya

--- Lesson11/src/Main.java
+++ Lesson11/src/Main.java
@@ -6,5 +6,13 @@ public class Main {
         vasya.gender = "m";
 
         System.out.println("Имя " + vasya.name + " возраст " + vasya.age + " пол " + vasya.gender);
+
+        Human petya = new Human();
+        petya.age = 13;
+        petya.name = "Петя";
+        petya.gender= "f";
+
+        System.out.println("Имя " + petya.name + " возраст " + petya.age + " пол " + petya.gender);
+
     }
 }
